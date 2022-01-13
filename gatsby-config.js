ab63f95c137b389ad40e65108bef9b680e5d1998
mariya1705 <65625954+mariya1705@users.noreply.github.com>
2022-01-13 21:52:37 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `https://strapi-portfolio-heroku.herokuapp.com`,
+        apiURL: `https://strapi-portfolio-mariya.herokuapp.com`,
         queryLimit: 10000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
 
